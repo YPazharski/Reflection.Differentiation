93e0381e70acb5ce635d5a2492e66a2080d5b1cf
YPazharski <136801415+YPazharski@users.noreply.github.com>
2023-06-16 13:26:35 +0300

    Добавьте файлы проекта.

---
+++ Algebra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Differentiation
+{
+    public static class Algebra
+    {
+        public static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> expression)
+        {
+
+        }
+    }
+}
