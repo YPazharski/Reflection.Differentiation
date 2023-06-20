5af3e62298b63a2b009779d729cff77981c245a7
YPazharski <136801415+YPazharski@users.noreply.github.com>
2023-06-20 12:34:30 +0300

    Added AlgebraAlt class - not mine alternative solution of the task

--- Algebra.cs
+++ Algebra.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace Reflection.Differentiation
 {
-
     public static class Algebra
     {
         /// <summary>
--- Algebra_should.cs
+++ Algebra_should.cs
@@ -19,71 +19,71 @@ namespace Reflection.Differentiation
             }
         }
 
-        //[Test, Order(1)]
-        //public void DifferentiateConstant()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => 42);
-        //}
+        [Test, Order(1)]
+        public void DifferentiateConstant()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => 42);
+        }
 
-        //[Test, Order(2)]
-        //public void DifferentiateParameter()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => z);
-        //}
+        [Test, Order(2)]
+        public void DifferentiateParameter()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => z);
+        }
 
-        //[Test, Order(3)]
-        //public void DifferentiateLinearFunction()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => z * 5);
-        //}
+        [Test, Order(3)]
+        public void DifferentiateLinearFunction()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => z * 5);
+        }
 
-        //[Test, Order(4)]
-        //public void DifferentiateQuadraticFunction()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => z * 5 * z);
-        //}
+        [Test, Order(4)]
+        public void DifferentiateQuadraticFunction()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => z * 5 * z);
+        }
 
-        //[Test, Order(5)]
-        //public void DifferentiateSum()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => z + z);
-        //}
+        [Test, Order(5)]
+        public void DifferentiateSum()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => z + z);
+        }
 
-        //[Test, Order(6)]
-        //public void DifferentiateSumAndProduct()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => 5 * z + z * z);
-        //}
+        [Test, Order(6)]
+        public void DifferentiateSumAndProduct()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => 5 * z + z * z);
+        }
 
-        //[Test, Order(7)]
-        //public void DifferentiateSin1()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => Math.Sin(z));
-        //}
+        [Test, Order(7)]
+        public void DifferentiateSin1()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => Math.Sin(z));
+        }
 
-        //[Test, Order(8)]
-        //public void DifferentiateSin2()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => Math.Sin(z * z + z));
-        //}
+        [Test, Order(8)]
+        public void DifferentiateSin2()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => Math.Sin(z * z + z));
+        }
 
-        //[Test, Order(9)]
-        //public void DifferentiateCos1()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => Math.Cos(z));
-        //}
+        [Test, Order(9)]
+        public void DifferentiateCos1()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => Math.Cos(z));
+        }
 
-        //[Test, Order(10)]
-        //public void DifferentiateCos2()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => Math.Cos(z * z + z));
-        //}
+        [Test, Order(10)]
+        public void DifferentiateCos2()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => Math.Cos(z * z + z));
+        }
 
-        //[Test, Order(11)]
-        //public void DifferentiateComplexExpression()
-        //{
-        //    AssertDerivativeEqualToNumericDerivative(z => Math.Cos(2 * z + z) + 2 * Math.Sin(3 * z + z) + Math.Sin(z + 1) * Math.Cos(z + 2) * 3);
-        //}
+        [Test, Order(11)]
+        public void DifferentiateComplexExpression()
+        {
+            AssertDerivativeEqualToNumericDerivative(z => Math.Cos(2 * z + z) + 2 * Math.Sin(3 * z + z) + Math.Sin(z + 1) * Math.Cos(z + 2) * 3);
+        }
 
         [Test, Order(12)]
         public void InformativeMessage_OnNotSupportedSyntax()
